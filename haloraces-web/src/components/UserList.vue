1bbf3ec663227be9bd41fbe83580fa56208f4b6f
DavidSpartan95 <davidulwhan@hotmail.com>
2025-04-09 15:31:49 +0200

    added all user names

--- haloraces-web/src/components/UserList.vue
+++ haloraces-web/src/components/UserList.vue
@@ -1,14 +1,16 @@
 <template>
-    <ul>
-      <li v-for="(user, index) in users" :key="index">
-        {{ user.name }} - Wins: {{ user.wins }}
-      </li>
-    </ul>
-  </template>
-  
-  <script setup lang="ts">
-  import { defineProps } from 'vue';
-  import { users } from '../data/users'; // import the User interface
-  
-  </script>
-  
+  <ul>
+    <li v-for="(user, index) in sortedUsers" :key="index">
+      {{ user.name }} - Wins: ???
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { users } from '../data/users'; // assume this is an array of user objects
+
+const sortedUsers = computed(() => {
+  return [...users].sort((a, b) => a.name.localeCompare(b.name));
+});
+</script>
