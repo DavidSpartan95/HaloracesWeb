1efcd1bec73914cbf90c0737d26afb9b8ee582ec
DavidSpartan95 <davidulwhan@hotmail.com>
2025-04-11 12:49:31 +0200

    gitpages update

--- haloraces-web/vite.config.ts
+++ haloraces-web/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+// Replace with your actual repo name!
+const repoName = 'HaloracesWeb'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [vue()],
 })
